0774c41b663e509bb3c5056c45ad4e3002542d37
kharod-g9185 <kharodg@student.douglascollege.ca>
2020-07-19 22:04:29 -0700

    commit 1 assignment 3

---
+++ gurmukh_9185_SEproject/gurmukh_9185_SEproject/src/HotelApp/common/HotelAppInterface.java
@@ -0,0 +1,8 @@
+package HotelApp.common;
+
+public interface HotelAppInterface {
+
+	public void clearFields(); //will have different clearing for each screen
+	public void initComponents();
+	public void createEvents();
+}
